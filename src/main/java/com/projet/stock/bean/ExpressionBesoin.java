553d59a45112b7be6133a5b4273817c5647fc24b
AKH <AKH@DESKTOP-9S5JDOO>
2020-02-12 15:20:36 +0100

    init project by akhadam

---
+++ src/main/java/com/projet/stock/bean/ExpressionBesoin.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.projet.stock.bean;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author AKH
+ */
+@Entity
+public class ExpressionBesoin implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String reference;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dateExpressionBesoin;
+    
+    @OneToOne
+    private EntiteAdministrative entiteAdministrative;
+    @OneToOne
+    private Personnel personnel;
+     
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ExpressionBesoin)) {
+            return false;
+        }
+        ExpressionBesoin other = (ExpressionBesoin) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.projet.stock.bean.ExpressionBesoin[ id=" + id + " ]";
+    }
+    
+}
